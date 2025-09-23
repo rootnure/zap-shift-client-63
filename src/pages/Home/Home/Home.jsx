8c4769fb56743b46a345853f3d16bba920749ebe
rootnure <nur.diu.2791@gmail.com>
2025-09-23 12:41:09 +0600

    assets and Home page, Navbar, Footer added

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Footer from '../../shared/Footer/Footer';
+import Navbar from '../../shared/Navbar/Navbar';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h1>This is Home</h1>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/shared/ZapFastLogo/ZapFastLogo.jsx
@@ -0,0 +1,12 @@
+import logo from "../../../assets/logo.png";
+
+const ZapFastLogo = () => {
+  return (
+    <div className="flex items-end">
+      <img className="mb-2" src={logo} alt="" />
+      <p className="text-3xl font-bold -ml-2">ZapFast</p>
+    </div>
+  );
+};
+
+export default ZapFastLogo;
